93546b6b9a7d86a8e21c5e0190b9755123671946
yangpanda <ypp13222204208@gmail.com>
2020-07-05 10:31:39 +0800

    7/5

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -59,13 +59,13 @@ class Handler extends ExceptionHandler
     public function render($request, Throwable $exception)
     {
         if ($exception->getPrevious() instanceof TokenExpiredException) {
-            return response()->json(['error' => '过期的token']);
+            return response()->json(['msg' => '过期的token','code'=>203]);
         } else if ($exception->getPrevious() instanceof TokenInvalidException) {
-            return response()->json(['error' => '无效的token']);
+            return response()->json(['msg' => '无效的token','code'=>204]);
         } else if ($exception->getPrevious() instanceof TokenBlacklistedException) {
             return response()->json(['error' => '列入黑名单']);
         } else {
-            return response()->json(['error' => "未经授权的请求"], 401);
+            return response()->json(['msg' => "未经授权的请求",'code'=>401], 401);
         }
         return parent::render($request, $exception);
     }

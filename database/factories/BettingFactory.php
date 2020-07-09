3f3be9edaf6246a818e9cf96750293a6b0333225
yangpanda <ypp13222204208@gmail.com>
2020-07-09 17:00:50 +0800

    7/9

--- database/factories/BettingFactory.php
+++ database/factories/BettingFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Betting::class, function (Faker $faker) {
     return [
         'username'=>$faker->phoneNumber,
-        'platform_name'=>$faker->randomElement(['taptap', '多玩', '大脚']),
+        'platform_name'=>$faker->randomElement(['亚博体育', 'IM体育', 'BG真人', 'AG真人', 'IM电竞']),
         'login_ip'=>$faker->ipv4,
         'game_name'=>$faker->randomElement(['wow', '大话西游', '梦幻西游']),
         'room_num'=>$faker->randomElement(['五号房', '六号房间', '八号房间']),
--- database/factories/PlatformFactory.php
+++ database/factories/PlatformFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Platform::class, function (Faker $faker) {
     return [
-        'platform_name'=>$faker->randomElement(['多玩', '大脚', 'taptap']),
+        'platform_name'=>$faker->randomElement(['亚博体育', 'IM体育', 'BG真人', 'AG真人', 'IM电竞']),
         'show_name'=>$faker->company,
         'platform_type'=>$faker->jobTitle,
         'platform_sort'=>$faker->randomDigitNotNull,

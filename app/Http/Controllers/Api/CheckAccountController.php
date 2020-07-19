f4c8fda27e69647a78beeeda4dab01aa52a049a5
yangpanda <ypp13222204208@gmail.com>
2020-07-19 14:50:27 +0800

    7/19

--- app/Http/Controllers/Api/CheckAccountController.php
+++ app/Http/Controllers/Api/CheckAccountController.php
@@ -102,7 +102,7 @@ class CheckAccountController extends Controller
                 'msg' => "token错误",
             ], 200);
         }
-        $data = $request->data[0];
+        $data = json_decode($request->data);
         $endround= new EndRound;
         $endround->account = $request->account;
         $endround->gamehall = $request->gamehall;

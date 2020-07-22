aaa21542ee966f46d1013a56098ae3c882162880
yangpanda <ypp13222204208@gmail.com>
2020-07-22 21:07:01 +0800

    f

--- app/Http/Controllers/Api/FastApiController.php
+++ app/Http/Controllers/Api/FastApiController.php
@@ -64,7 +64,7 @@ class FastApiController extends Controller
         $data['MemberAccount'] = $request->MemberAccount;
         $data['MemberPassword'] = $request->MemberPassword;
         $data['Hash'] = $request->Hash;
-        $data = json_encode($data);return $_SERVER["REMOTE_ADDR"];
+        //$data = json_encode($data);return $_SERVER["REMOTE_ADDR"];
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $request->url);//要访问的地址
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);//执行结果是否被返回，0是返回，1是不返回

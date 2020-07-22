671edc225200fad90df8891b40890a93a36592d3
yangpanda <ypp13222204208@gmail.com>
2020-07-22 21:40:03 +0800

    f

--- app/Http/Controllers/Api/FastApiController.php
+++ app/Http/Controllers/Api/FastApiController.php
@@ -9,22 +9,44 @@ use App\Http\Controllers\Controller;
 class FastApiController extends Controller
 {
 
-    public function curlData($url,$data)
-    {
+    public function curlData($url,$data,$method = 'POST')
+    {  return $url;
+        //初始化
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json'
-        ));
-        curl_setopt($ch, CURLOPT_URL, $url);//要访问的地址
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);//执行结果是否被返回，0是返回，1是不返回
-        curl_setopt($ch, CURLOPT_POST, 1);// 发送一个常规的POST请求
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_exec($ch);//执行并获取数据
+        $headers = ['Content-Type: application/json'];
+        if($method == 'GET'){
+            $querystring = http_build_query($data);
+            $url = $url.'?'.$querystring;
+        }
+        // 请求头，可以传数组
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // 执行后不直接打印出来
+        if($method == 'POST'){
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST,'POST');     // 请求方式
+            curl_setopt($ch, CURLOPT_POST, true);        // post提交
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);   // post的变量
+
+        }
+        if($method == 'PUT'){
+            curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+            curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+        }
+        if($method == 'DELETE'){
+            curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+            curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+        }
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // 不从证书中检查SSL加密算法是否存在
+        $output = curl_exec($ch); //执行并获取HTML文档内容
+        curl_close($ch); //释放curl句柄
+        return $output;
     }
 
     public function register(Request $request)
     {
-        $this->validate($request, [
+/*         $this->validate($request, [
             'token' => 'required'
         ]);
 
@@ -34,7 +56,7 @@ class FastApiController extends Controller
                 'code' => 0,
                 'msg' => '用户名错误',
             ], 200);
-        }
+        } */
         $data = array();
         $data['ApiKey']= $request->ApiKey;
         $data['Timestamp'] = intval($request->Timestamp);
@@ -43,7 +65,17 @@ class FastApiController extends Controller
         $data['MemberPassword'] = $request->MemberPassword;
         $data['Hash'] = $request->Hash;
         $data = json_encode($data);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json'
+        ));
+        curl_setopt($ch, CURLOPT_URL, $request->url);//要访问的地址
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);//执行结果是否被返回，0是返回，1是不返回
+        curl_setopt($ch, CURLOPT_POST, 1);// 发送一个常规的POST请求
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        $output = curl_exec($ch);//执行并获取数据
 
-        $this->curlData($request->url,$data);
+        //return response()->json(['data'=>$output]);
+
     }
 }

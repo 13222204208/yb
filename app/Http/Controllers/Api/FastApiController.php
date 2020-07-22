2784b2ed573859a6b08206bce04076912e995b8e
yangpanda <ypp13222204208@gmail.com>
2020-07-22 21:50:56 +0800

    f

--- app/Http/Controllers/Api/FastApiController.php
+++ app/Http/Controllers/Api/FastApiController.php
@@ -29,12 +29,12 @@ class FastApiController extends Controller
         ]);
 
         $user= JWTAuth::authenticate($request->token);
-        if ($user->username != $request->MemberAccount) {
+   /*      if ($user->username != $request->MemberAccount) {
             return response()->json([
                 'code' => 0,
                 'msg' => '用户名错误',
             ], 200);
-        }
+        } */
         $data = array();
         $data['ApiKey']= $request->ApiKey;
         $data['Timestamp'] = intval($request->Timestamp);
@@ -44,14 +44,8 @@ class FastApiController extends Controller
         $data['Hash'] = $request->Hash;
         $data = json_encode($data);
         $url = $request->url;
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json'
-        ));
-        curl_setopt($ch, CURLOPT_URL, $url);//要访问的地址
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);//执行结果是否被返回，0是返回，1是不返回
-        curl_setopt($ch, CURLOPT_POST, 1);// 发送一个常规的POST请求
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_exec($ch);//执行并获取数据
+
+        $this->curlData($url,$data);
+
     }
 }

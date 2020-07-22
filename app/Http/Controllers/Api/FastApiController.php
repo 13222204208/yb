5bcc8629340f437cb56eef58ee11fd4be7c72461
yangpanda <ypp13222204208@gmail.com>
2020-07-22 19:32:20 +0800

    f

--- app/Http/Controllers/Api/FastApiController.php
+++ app/Http/Controllers/Api/FastApiController.php
@@ -8,25 +8,22 @@ use App\Http\Controllers\Controller;
 
 class FastApiController extends Controller
 {
-    public function curl_post_https($url,$data){ // 模拟提交数据函数
-        $curl = curl_init(); // 启动一个CURL会话
-        curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址
-        //curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
-        //curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1); // 从证书中检查SSL加密算法是否存在
-        //curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
-        //curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
-        curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
+    public function curl_post_https($url,$jsonStr){ // 模拟提交数据函数
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonStr);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json; charset=utf-8',
+                'Content-Length: ' . strlen($jsonStr)
+            )
+        );
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
 
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 0);//执行结果是否被返回，0是返回，1是不返回
-        curl_setopt($curl, CURLOPT_POST, 1);// 发送一个常规的POST请求
-        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-
-        $tmpInfo = curl_exec($curl); // 执行操作
-        if (curl_errno($curl)) {
-            echo 'Errno'.curl_error($curl);//捕抓异常
-        }
-        curl_close($curl); // 关闭CURL会话
-        return $tmpInfo; // 返回数据，json格式
+        return array($httpCode, $response);
     }
 
     public function register(Request $request)

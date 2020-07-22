82b15ff51839e21e9b957ea9cbfa905f7e71fa65
yangpanda <ypp13222204208@gmail.com>
2020-07-22 19:01:35 +0800

    f

--- app/Http/Controllers/Api/FastApiController.php
+++ app/Http/Controllers/Api/FastApiController.php
@@ -16,11 +16,11 @@ class FastApiController extends Controller
         //curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
         //curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
         curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
-        curl_setopt($curl, CURLOPT_POST, 1); // 发送一个常规的Post请求
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data); // Post提交的数据包
-        curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
-        curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 0);//执行结果是否被返回，0是返回，1是不返回
+        curl_setopt($curl, CURLOPT_POST, 1);// 发送一个常规的POST请求
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+
         $tmpInfo = curl_exec($curl); // 执行操作
         if (curl_errno($curl)) {
             echo 'Errno'.curl_error($curl);//捕抓异常

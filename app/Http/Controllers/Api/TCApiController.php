05bb60c8e7b708e5916739d1edfee1550151175d
yangpanda <ypp13222204208@gmail.com>
2020-07-23 21:48:29 +0800

    tc

--- app/Http/Controllers/Api/TCApiController.php
+++ app/Http/Controllers/Api/TCApiController.php
@@ -12,11 +12,24 @@ class TCApiController extends Controller
     public $signkey = 'tw3947BNYH3Y1pn9';
     public $url = 'http://www.connect6play.com/doBusiness.do';
 
+    public function curlData($url,$data)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json'
+        ));
+        curl_setopt($ch, CURLOPT_URL, $url);//要访问的地址
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);//执行结果是否被返回，0是返回，1是不返回
+        curl_setopt($ch, CURLOPT_POST, 1);// 发送一个常规的POST请求
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_exec($ch);//执行并获取数据
+    }
+
     function encryptText($str, $key)
     {
-        $str = $this->pkcsPadding($str, 8);
+        //$str = $this->pkcsPadding($str, 8);
         $data = openssl_encrypt($str, 'DES-ECB', $key);    // 加密
-        return base64_encode($data);
+        return $data;
     }
 
     private function pkcsPadding($str, $blocksize)
@@ -31,8 +44,7 @@ class TCApiController extends Controller
         $params =  $this->encryptText($sendParams,$this->desKey);//echo $this->signkey; exit;
         //echo $params; exit;
         $sign = hash('sha256',$params. $this->signkey);//echo $sign;exit;
-        $params='U2FsdGVkX1+0Jgg2disd3pdKaILT2xzJAbF9HoFg20MW+PSvZCaP1FkOgi8caNxBXVN4SxNrhSO2r+WHeM9XzmZNLRKDqqzrGoAFzPlCrKIH/URmoGmF/X4N6cOrZ4Kq';
-        $sign='3a7fcf1dbcd919e7baeae1e862bdfe74c5c7abf3f147f50fc027d7cf2e055d57';
+        //echo $params.'//'.$sign;exit;
         $data = array('merchant_code' => $this->merchant_code, 'params' => $params , 'sign' => $sign);
 
         $options = array(
@@ -44,8 +56,9 @@ class TCApiController extends Controller
         );
         $context  = stream_context_create($options);
         $result = file_get_contents($this->url, false, $context);
+        $this->curlData($this->url,$context);
         //dd($result);
-        return $result;
+       // return $result;
     }
 
 

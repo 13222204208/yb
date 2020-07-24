aaa9c8b35aa8d6049d64138c37cc6d6cd4c3741e
yangpanda <ypp13222204208@gmail.com>
2020-07-24 16:55:51 +0800

    7/24

--- app/Http/Controllers/Api/TCApiController.php
+++ app/Http/Controllers/Api/TCApiController.php
@@ -50,12 +50,13 @@ class TCApiController extends Controller
 
 
     public function send_require($sendParams){
-       // $params =  $this->encryptText(json_encode($sendParams),$this->desKey);
-$params ="qJ153L0WjoxYO2G2SQ2tA%2Bn5ZW75%2FFH6llz2WevLLfk4jA2Gpblts8BGnmMeY7Xks0tPaPA0iZFwkvX9EnVmofO0N97LLzadNNZ4ivyPDvA%3D";
-/*          $params= urlencode(mb_convert_encoding($params, 'utf-8', 'gb2312'));
-echo $params; */
-       // $sign = hash('sha256', $params . $this->signKey);//echo $sign;exit;
-        $sign ="d1778d4fe33f67caa4ec6fafad836b4bedfc8b24e4727b538c32b7f53572277c";
+        $params =  $this->encryptText(json_encode($sendParams),$this->desKey);
+//$params ="qJ153L0WjoxYO2G2SQ2tA%2Bn5ZW75%2FFH6llz2WevLLfk4jA2Gpblts8BGnmMeY7Xks0tPaPA0iZFwkvX9EnVmofO0N97LLzadNNZ4ivyPDvA%3D";
+
+
+        $sign = hash('sha256', $params . $this->signKey);//echo $sign;exit;
+        $params= urlencode(mb_convert_encoding($params, 'utf-8', 'gb2312'));
+        //$sign ="d1778d4fe33f67caa4ec6fafad836b4bedfc8b24e4727b538c32b7f53572277c";
         $data = array('merchant_code' => $this->merchant_code, 'params' => $params , 'sign' => $sign);
         //dd($data);
         $this->curlData($this->url,$data);
@@ -77,8 +78,8 @@ echo $params; */
     {
         $data = array();
         $data['method']='cm';
-        $data['username']= 'yangpanda';
-        $data['password']= 'yangpanda';
+        $data['username']= 'yangpanda1';
+        $data['password']= 'yangpanda1';
         $data['currency']= 'CNY';
         //$data = json_encode($data);
         $result = $this->send_require($data);

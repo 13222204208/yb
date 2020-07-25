bd01732f485a2f916dfcfd30433e00c5d1ff11f0
yangpanda <ypp13222204208@gmail.com>
2020-07-25 14:27:37 +0800

    7/25

--- app/Http/Controllers/Api/YBApiController.php
+++ app/Http/Controllers/Api/YBApiController.php
@@ -55,9 +55,9 @@ class YBApiController extends Controller
         curl_setopt($ch, CURLOPT_URL, $url);//要访问的地址
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);//执行结果是否被返回，0是返回，1是不返回
         curl_setopt($ch, CURLOPT_POST, 1);// 发送一个常规的POST请求
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($param));
-        //curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_exec($ch);//执行并获取数据
         curl_close($ch);
     }

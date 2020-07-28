ad125654786334a455852c3b8849b27332e20ca9
yangpanda <ypp13222204208@gmail.com>
2020-07-28 19:05:37 +0800

    cp

--- app/Http/Controllers/Api/YBCPApiController.php
+++ app/Http/Controllers/Api/YBCPApiController.php
@@ -173,7 +173,7 @@ class YBCPApiController extends Controller
         $data['sign']=md5('endTime'.$data['endTime'].'member'.$data['member'].'merchant'.$data['merchant'].'notifyId'.$data['notifyId'].'pageSize'.$data['pageSize'].'pageNum'.$data['pageNum'].'startTime'.$data['startTime'].'timestamp'.$data['timestamp'].'tradeType'.$data['tradeType'].$this->signKey);
 
         $type= array("Content-Type:application/json","User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
-return json_encode($data);
+
         $url=$request->url;
         $this->curlData($url,json_encode($data),$type);
     }

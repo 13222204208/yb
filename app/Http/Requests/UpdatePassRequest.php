2a7ccadaf39cd594beb4941445f3ee79e49ed0ca
yangpanda <ypp13222204208@gmail.com>
2020-08-03 13:45:50 +0800

    8/3

--- app/Http/Requests/UpdatePassRequest.php
+++ app/Http/Requests/UpdatePassRequest.php
@@ -10,18 +10,18 @@ class UpdatePassRequest extends ApiBaseRequest
     /**
      * 获取应用于请求的验证规则
      *
-     * 
+     *
      */
     public function rules()
     {
         return [
             'newpass' => 'required|min:6|max:12|alpha_num',
-            'password' => 'required|min:6|max:12|alpha_num',
+            'password' => 'required|min:6|max:12|alpha_num|regex:/^(?!([A-Za-z]+|d\d+)$)[A-Za-z\d]$/',
         ];
     }
 
     public function messages()
-    {  
+    {
        return [
             'newpass.alpha_num' => '只允许字母和数字',
             'password.alpha_num'=> '只允许字母和数字',

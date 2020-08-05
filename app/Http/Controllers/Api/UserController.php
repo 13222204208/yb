f66cdd99a0ad541d9b5eb1867264b485924c8273
yangpanda <ypp13222204208@gmail.com>
2020-08-05 14:01:36 +0800

    8/5

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -100,10 +100,14 @@ class UserController extends Controller
     }
 
 
-    public function update(UserDetailAuthRequest $request)
+    public function update(Request $request)
     {
         $this->validate($request, [
-            'token' => 'required'
+            'token' => 'required',
+            'true_name' => 'regex:/^[\x{4e00}-\x{9fa5}]{2,4}$/u',
+            'email' => 'email',
+            'phone' =>'regex:/^1[345789][0-9]{9}$/',
+            'date_brith' => 'date'
         ]);
 
         $user = JWTAuth::authenticate($request->token);

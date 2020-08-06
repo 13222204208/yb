1e5ee3d02189e4b12f0b265490396ecccc962b59
yangpanda <ypp13222204208@gmail.com>
2020-08-06 14:03:26 +0800

    8/6

--- app/Http/Controllers/Api/FastApiController.php
+++ app/Http/Controllers/Api/FastApiController.php
@@ -223,11 +223,11 @@ class FastApiController extends Controller
         $data['StartDate'] = $StartDate;
         $data['EndDate'] = $EndDate;
         $data['Hash'] = MD5($this->apiKey.$data['Game'].$data['StartDate'].$data['EndDate'].$this->apiSecret.$data['Timestamp']);
-        $data = json_encode($data);
+        //$data = json_encode($data);
         $url = 'http://api.test.fastapi2020.com:6080/Api/Game/BetRecord';
 
         //$data = stripslashes(json_encode($data,JSON_UNESCAPED_UNICODE));
-$str= $this->apiKey;
+$str= $data['ApiKey'].$data['Game'].$data['StartDate'].$data['EndDate'].$this->apiSecret.$data['Timestamp'];
 return $str;
 
         $result= $this->curlData($url,$data);

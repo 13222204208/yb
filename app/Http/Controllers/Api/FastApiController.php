d88aea658d5a31682e0c63ad0f009e56b34d9e8f
yangpanda <ypp13222204208@gmail.com>
2020-08-06 11:42:17 +0800

    8/6

--- app/Http/Controllers/Api/FastApiController.php
+++ app/Http/Controllers/Api/FastApiController.php
@@ -227,7 +227,7 @@ class FastApiController extends Controller
         $url = 'http://api.test.fastapi2020.com:6080/Api/Game/BetRecord';
 
         $data = json_encode($data,JSON_UNESCAPED_SLASHES);
-return $data;
+
         $result= $this->curlData($url,$data);
         return $result;
     }

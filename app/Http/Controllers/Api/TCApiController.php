5a340302f3ebc93b03d63e1d2b2f3fa0f461728d
yangpanda <ypp13222204208@gmail.com>
2020-08-07 21:54:30 +0800

    8/7

--- app/Http/Controllers/Api/TCApiController.php
+++ app/Http/Controllers/Api/TCApiController.php
@@ -270,7 +270,7 @@ class TCApiController extends Controller
         $game= array('pvpbd','bd');
 
 
-        $time= date('YmdHi',time()-45*60);
+        $time= date('YmdHi',time()-55*60);
 
         $num= intval(substr($time,10));
         if ($num >= 0 && $num < 15) $batch_name = substr($time,0,-2).'00';
@@ -283,7 +283,7 @@ class TCApiController extends Controller
             $data['method'] = $game[$i];
             $data['batch_name'] = $batch_name;
             $result = $this->send_require($data);
-            return gettype($result);
+            return $result;
             $record = json_decode($result,true); return $record['details'];
             DB::table('tc_pvpbd_record')->insert($record['details']);
 

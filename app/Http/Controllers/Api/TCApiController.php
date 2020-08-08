7bb8db3ec0d6fc434f910c2552fb28e7ab725d1d
yangpanda <ypp13222204208@gmail.com>
2020-08-08 11:48:32 +0800

    8/7

--- app/Http/Controllers/Api/TCApiController.php
+++ app/Http/Controllers/Api/TCApiController.php
@@ -283,23 +283,12 @@ class TCApiController extends Controller
             $data['method'] = $game[$i];
             $data['batch_name'] = '202008072015';
             $result = $this->send_require($data);
-            header('content-type:application/json;charset=utf8');
-
-
-           $record = json_decode($result,true);
-
-/* return $record['details'][0]['gameCode'].$record['details'][0]['username'];
-            //return $record['details'][0];
-            //return gettype($record['details'][0]['additionalInfo']);
-            DB::table('tc_pvpbd_record')->insert(['gameCode'=>$record['details'][0]['gameCode'],'username'=>$record['details'][0]['username']]); */
-
-            DB::table('tc_pvpbd_record')->insert(
-                ['gameCode' => '12323', 'username' => 'adf123']
-            );
+            $record = json_decode($result, true);
 
             if ($record['details'] != null && $record['status'] === 0) {
                 $tableName = 'tc_'.$game[$i].'_record';//拼接数据表名,插入数据
                 DB::table($tableName)->insert($record['details']);
+                return false;
             }
         }
     }

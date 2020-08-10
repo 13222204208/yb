ebd60081118392c554177fd09a8bb22b91afd849
yangpanda <ypp13222204208@gmail.com>
2020-08-10 15:47:08 +0800

    8/10

--- app/Http/Controllers/Api/PlatformController.php
+++ app/Http/Controllers/Api/PlatformController.php
@@ -39,43 +39,49 @@ class PlatformController extends Controller
 
 
         if ($request->start_time && $request->stop_time) {
-            $game= array('AG','BBIN','OGPlus','AllBet','EG','WM','AVIA','IMSB','LC','VR','ThreeSing','SABA');
+            $game = array('AG', 'BBIN', 'OGPlus', 'AllBet', 'EG', 'WM', 'AVIA', 'IMSB', 'LC', 'VR', 'ThreeSing', 'SABA');
 
-            if (in_array($platform_name,$game)) {
-                $tableName = 'aq_'.strtolower($platform_name).'_record';
-                $data = DB::table($tableName)->orderBy('BetDate', 'desc')->where('MemberAccount', $user->username)->whereDate('BetDate', '>=',$request->start_time)->whereDate('BetDate', '<=',$request->stop_time)->get(
+            if (in_array($platform_name, $game)) {
+                $tableName = 'aq_' . strtolower($platform_name) . '_record';
+                $data = DB::table($tableName)->orderBy('BetDate', 'desc')->where('MemberAccount', $user->username)->whereDate('BetDate', '>=', $request->start_time)->whereDate('BetDate', '<=', $request->stop_time)->get(
                     ['id', 'BetStatus', 'TotalWinlose', 'Bet', 'TotalPayout', 'BetDate']
                 );
 
                 $todayCount = DB::table($tableName)->orderBy('BetDate', 'desc')->where('MemberAccount', $user->username)->whereBetween('BetDate', [$request->start_time, $request->stop_time])->selectRaw('DATE_FORMAT(BetDate,"%m-%d") as date,COUNT(id) as num ,SUM(Bet) as Bet,SUM(TotalPayout) as TotalPayout')
                     ->groupBy('date')->get();
 
-                    $allCount = DB::table($tableName)->orderBy('BetDate', 'desc')->where('MemberAccount', $user->username)->whereBetween('BetDate', [$request->start_time, $request->stop_time])->selectRaw('COUNT(id) as num ,SUM(Bet) as Bet,SUM(TotalPayout) as TotalPayout')
-                        ->get();
+                $allCount = DB::table($tableName)->orderBy('BetDate', 'desc')->where('MemberAccount', $user->username)->whereBetween('BetDate', [$request->start_time, $request->stop_time])->selectRaw('COUNT(id) as num ,SUM(Bet) as Bet,SUM(TotalPayout) as TotalPayout')
+                    ->get();
             }
 
-            if ($platform_name == 'ybqp') {//亚博棋牌投注记录
-                $data = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->whereDate('st', '>=',$request->start_time)->whereDate('st', '<=',$request->stop_time)->get(
+            if ($platform_name == 'ybqp') { //亚博棋牌投注记录
+                $data = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->whereDate('st', '>=', $request->start_time)->whereDate('st', '<=', $request->stop_time)->get(
                     ['id', 'gn', 'mw', 'bc', 'tb', 'st']
                 );
 
                 $todayCount = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->whereBetween('st', [$request->start_time, $request->stop_time])->selectRaw('DATE_FORMAT(st,"%m-%d") as date,COUNT(id) as num ,SUM(tb) as tb,SUM(mw) as mw')
                     ->groupBy('date')->get();
 
-                    $allCount = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->whereBetween('st', [$request->start_time, $request->stop_time])->selectRaw('COUNT(id) as num ,SUM(tb) as tb,SUM(mw) as mw')
-                        ->get();
+                $allCount = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->whereBetween('st', [$request->start_time, $request->stop_time])->selectRaw('COUNT(id) as num ,SUM(tb) as tb,SUM(mw) as mw')
+                    ->get();
             }
 
-            if ($platform_name == 'bd') {//天成电子游戏投注记录
-                $data = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->whereDate('st', '>=',$request->start_time)->whereDate('st', '<=',$request->stop_time)->get(
-                    ['id', 'gn', 'mw', 'bc', 'tb', 'st']
+            if ($platform_name == 'bd' && $request->has('productType')) { //天成电子游戏投注记录
+                $productType = $request->productType;
+                $data = DB::table('tc_bd_record')->orderBy('betTime', 'desc')->where('username', $user->username)->where('productType',$productType)->whereDate('betTime', '>=', $request->start_time)->whereDate('betTime', '<=', $request->stop_time)->get(
+                    ['id', 'betAmount', 'winAmount', 'netPnl', 'betTime']
                 );
 
-                $todayCount = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->whereBetween('st', [$request->start_time, $request->stop_time])->selectRaw('DATE_FORMAT(st,"%m-%d") as date,COUNT(id) as num ,SUM(tb) as tb,SUM(mp) as mp')
+                $todayCount = DB::table('tc_bd_record')->orderBy('betTime', 'desc')->where('username', $user->username)->whereBetween('betTime', [$request->start_time, $request->stop_time])->selectRaw('DATE_FORMAT(betTime
+                ,"%m-%d") as date,COUNT(id) as num ,SUM(betAmount
+                ) as betAmount
+                ,SUM(netPnl) as netPnl')
                     ->groupBy('date')->get();
 
-                    $allCount = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->whereBetween('st', [$request->start_time, $request->stop_time])->selectRaw('COUNT(id) as num ,SUM(tb) as tb,SUM(mp) as mp')
-                        ->get();
+                $allCount = DB::table('tc_bd_record')->orderBy('betTime', 'desc')->where('username', $user->username)->whereBetween('betTime', [$request->start_time, $request->stop_time])->selectRaw('COUNT(id) as num ,SUM(betAmount
+                    ) as betAmount
+                    ,SUM(netPnl) as netPnl')
+                    ->get();
             }
 
 
@@ -93,7 +99,6 @@ class PlatformController extends Controller
                     'msg' => "无数据",
                 ], 200);
             }
-
         }
 
         $btime = date('Y-m-d H:i:s', time() - 7 * 24 * 60 * 60);
@@ -105,70 +110,67 @@ class PlatformController extends Controller
             }
         }
 
-        $game= array('AG','BBIN','OGPlus','AllBet','EG','WM','AVIA','IMSB','LC','VR','ThreeSing','SABA');
+        $game = array('AG', 'BBIN', 'OGPlus', 'AllBet', 'EG', 'WM', 'AVIA', 'IMSB', 'LC', 'VR', 'ThreeSing', 'SABA');
 
-    if (in_array($platform_name,$game)) {
-        $tableName = 'aq_'.strtolower($platform_name).'_record';
-        $data = DB::table($tableName)->orderBy('BetDate', 'desc')->where('MemberAccount', $user->username)->whereDate('BetDate', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
-            $query->whereDate('BetDate', '=', $yesterday);
-        })->get(
-            ['id', 'BetStatus', 'TotalWinlose', 'Bet', 'TotalPayout', 'BetDate']
-        );
+        if (in_array($platform_name, $game)) {
+            $tableName = 'aq_' . strtolower($platform_name) . '_record';
+            $data = DB::table($tableName)->orderBy('BetDate', 'desc')->where('MemberAccount', $user->username)->whereDate('BetDate', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
+                $query->whereDate('BetDate', '=', $yesterday);
+            })->get(
+                ['id', 'BetStatus', 'TotalWinlose', 'Bet', 'TotalPayout', 'BetDate']
+            );
 
-        $todayCount =DB::table($tableName)->orderBy('BetDate', 'desc')->where('MemberAccount', $user->username)->whereDate('BetDate', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
-            $query->whereDate('BetDate', '=', $yesterday);
-        })->selectRaw('DATE_FORMAT(BetDate,"%m-%d") as date,COUNT(id) as num ,SUM(Bet) as Bet,SUM(TotalPayout) as TotalPayout')
-            ->groupBy('date')->get();
+            $todayCount = DB::table($tableName)->orderBy('BetDate', 'desc')->where('MemberAccount', $user->username)->whereDate('BetDate', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
+                $query->whereDate('BetDate', '=', $yesterday);
+            })->selectRaw('DATE_FORMAT(BetDate,"%m-%d") as date,COUNT(id) as num ,SUM(Bet) as Bet,SUM(TotalPayout) as TotalPayout')
+                ->groupBy('date')->get();
 
-        $allCount = DB::table($tableName)->orderBy('BetDate', 'desc')->where('MemberAccount', $user->username)->whereDate('BetDate', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
-            $query->whereDate('bottom_pour_time', '=', $yesterday);
-        })->selectRaw('COUNT(id) as num ,SUM(Bet) as Bet,SUM(TotalPayout) as TotalPayout')
-            ->get();
-    }
-
-    if ($platform_name == 'ybqp') {
-        $btime = time() - 7 * 24 * 60 * 60;
-        $yesterday = "";
-        if ($request->has('day')) {
-            $btime = date('Y-m-d H:i:s', time() - $request->day * 24 * 60 * 60);
-            if ($request->day == 2) {
-                $yesterday = Carbon::yesterday();
-            }
+            $allCount = DB::table($tableName)->orderBy('BetDate', 'desc')->where('MemberAccount', $user->username)->whereDate('BetDate', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
+                $query->whereDate('bottom_pour_time', '=', $yesterday);
+            })->selectRaw('COUNT(id) as num ,SUM(Bet) as Bet,SUM(TotalPayout) as TotalPayout')
+                ->get();
         }
-        $data = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->where('st', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
-            $query->whereDate('st', '=', $yesterday);
-        })->get(
-            ['id', 'gn', 'mw', 'bc', 'tb', 'st']
-        );
 
-        $todayCount = YbChessRecord::where('mmi', $user->username)->where('st', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
-            $query->whereDate('st', '=', $yesterday);
-        })->selectRaw('DATE_FORMAT(st,"%m-%d") as date,COUNT(id) as num ,SUM(tb) as tb,SUM(mw) as mw')
-            ->groupBy('date')->get();
+        if ($platform_name == 'ybqp') {
+            $btime = time() - 7 * 24 * 60 * 60;
+            $yesterday = "";
+            if ($request->has('day')) {
+                $btime = date('Y-m-d H:i:s', time() - $request->day * 24 * 60 * 60);
+                if ($request->day == 2) {
+                    $yesterday = Carbon::yesterday();
+                }
+            }
+            $data = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->where('st', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
+                $query->whereDate('st', '=', $yesterday);
+            })->get(
+                ['id', 'gn', 'mw', 'bc', 'tb', 'st']
+            );
 
-        $allCount = YbChessRecord::where('mmi', $user->username)->where('st', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
-            $query->whereDate('st', '=', $yesterday);
-        })->selectRaw('COUNT(id) as num ,SUM(tb) as tb,SUM(mw) as mw')
-            ->get();
+            $todayCount = YbChessRecord::where('mmi', $user->username)->where('st', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
+                $query->whereDate('st', '=', $yesterday);
+            })->selectRaw('DATE_FORMAT(st,"%m-%d") as date,COUNT(id) as num ,SUM(tb) as tb,SUM(mw) as mw')
+                ->groupBy('date')->get();
 
-    }
-
-
-    if ($data) {
-        return response()->json([
-            'msg' => '成功',
-            'data' => $data,
-            'allCount' => $allCount,
-            'todayCount' => $todayCount,
-            'code' => 200
-        ]);
-    } else {
-        return response()->json([
-            'code' => 0,
-            'msg' => "无数据",
-        ], 200);
-    }
+            $allCount = YbChessRecord::where('mmi', $user->username)->where('st', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
+                $query->whereDate('st', '=', $yesterday);
+            })->selectRaw('COUNT(id) as num ,SUM(tb) as tb,SUM(mw) as mw')
+                ->get();
+        }
 
 
+        if ($data) {
+            return response()->json([
+                'msg' => '成功',
+                'data' => $data,
+                'allCount' => $allCount,
+                'todayCount' => $todayCount,
+                'code' => 200
+            ]);
+        } else {
+            return response()->json([
+                'code' => 0,
+                'msg' => "无数据",
+            ], 200);
+        }
     }
 }

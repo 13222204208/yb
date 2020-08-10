77b5ac32cb0e236bac74a9f11455e62ef31a02fe
yangpanda <ypp13222204208@gmail.com>
2020-08-10 14:34:32 +0800

    8/10

--- app/Http/Controllers/Api/PlatformController.php
+++ app/Http/Controllers/Api/PlatformController.php
@@ -54,7 +54,19 @@ class PlatformController extends Controller
                         ->get();
             }
 
-            if ($platform_name == 'ybqp') {
+            if ($platform_name == 'ybqp') {//亚博棋牌投注记录
+                $data = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->whereDate('st', '>=',$request->start_time)->whereDate('st', '<=',$request->stop_time)->get(
+                    ['id', 'gn', 'mw', 'bc', 'tb', 'st']
+                );
+
+                $todayCount = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->whereBetween('st', [$request->start_time, $request->stop_time])->selectRaw('DATE_FORMAT(st,"%m-%d") as date,COUNT(id) as num ,SUM(tb) as tb,SUM(mw) as mw')
+                    ->groupBy('date')->get();
+
+                    $allCount = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->whereBetween('st', [$request->start_time, $request->stop_time])->selectRaw('COUNT(id) as num ,SUM(tb) as tb,SUM(mw) as mw')
+                        ->get();
+            }
+
+            if ($platform_name == 'bd') {//天成电子游戏投注记录
                 $data = YbChessRecord::orderBy('st', 'desc')->where('mmi', $user->username)->whereDate('st', '>=',$request->start_time)->whereDate('st', '<=',$request->stop_time)->get(
                     ['id', 'gn', 'mw', 'bc', 'tb', 'st']
                 );
@@ -66,21 +78,6 @@ class PlatformController extends Controller
                         ->get();
             }
 
-/*             $data = Betting::orderBy('bottom_pour_time', 'desc')->where('username', $user->username)->whereDate('bottom_pour_time', '>=',$request->start_time)->whereDate('bottom_pour_time', '<=',$request->stop_time)->when($platform_name, function ($query) use ($platform_name) {
-                $query->where('platform_name', '=', $platform_name);
-            })->get(
-                ['id', 'platform_name', 'game_name', 'bottom_pour', 'group_money', 'bottom_pour_time']
-            );
-
-            $todayCount = Betting::orderBy('bottom_pour_time', 'desc')->where('username', $user->username)->whereBetween('bottom_pour_time', [$request->start_time, $request->stop_time])->when($platform_name, function ($query) use ($platform_name) {
-                $query->where('platform_name', '=', $platform_name);
-            })->selectRaw('DATE_FORMAT(bottom_pour_time,"%m-%d") as date,COUNT(id) as num ,SUM(bottom_pour) as bottom_pour,SUM(group_money) as group_money')
-                ->groupBy('date')->get();
-
-            $allCount = Betting::orderBy('bottom_pour_time', 'desc')->where('username', $user->username)->whereBetween('bottom_pour_time', [$request->start_time, $request->stop_time])->when($platform_name, function ($query) use ($platform_name) {
-                $query->where('platform_name', '=', $platform_name);
-            })->selectRaw('COUNT(id) as num ,SUM(bottom_pour) as bottom_pour,SUM(group_money) as group_money')
-                ->get(); */
 
             if ($data) {
                 return response()->json([
@@ -97,14 +94,6 @@ class PlatformController extends Controller
                 ], 200);
             }
 
-            if ($data) {
-                return response()->json(['msg' => '成功', 'data' => $data, 'code' => 200]);
-            } else {
-                return response()->json([
-                    'code' => 0,
-                    'msg' => "无数据",
-                ], 200);
-            }
         }
 
         $btime = date('Y-m-d H:i:s', time() - 7 * 24 * 60 * 60);
@@ -135,21 +124,6 @@ class PlatformController extends Controller
             $query->whereDate('bottom_pour_time', '=', $yesterday);
         })->selectRaw('COUNT(id) as num ,SUM(Bet) as Bet,SUM(TotalPayout) as TotalPayout')
             ->get();
-
-        if ($data) {
-            return response()->json([
-                'msg' => '成功',
-                'data' => $data,
-                'allCount' => $allCount,
-                'todayCount' => $todayCount,
-                'code' => 200
-            ]);
-        } else {
-            return response()->json([
-                'code' => 0,
-                'msg' => "无数据",
-            ], 200);
-        }
     }
 
     if ($platform_name == 'ybqp') {
@@ -169,65 +143,32 @@ class PlatformController extends Controller
 
         $todayCount = YbChessRecord::where('mmi', $user->username)->where('st', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
             $query->whereDate('st', '=', $yesterday);
-        })->selectRaw('DATE_FORMAT(st,"%m-%d") as date,COUNT(id) as num ,SUM(tb) as tb,SUM(mp) as mp')
+        })->selectRaw('DATE_FORMAT(st,"%m-%d") as date,COUNT(id) as num ,SUM(tb) as tb,SUM(mw) as mw')
             ->groupBy('date')->get();
 
         $allCount = YbChessRecord::where('mmi', $user->username)->where('st', '>', $btime)->when($yesterday, function ($query) use ($yesterday) {
             $query->whereDate('st', '=', $yesterday);
-        })->selectRaw('COUNT(id) as num ,SUM(tb) as tb,SUM(mp) as mp')
+        })->selectRaw('COUNT(id) as num ,SUM(tb) as tb,SUM(mw) as mw')
             ->get();
 
-        if ($data) {
-            return response()->json([
-                'msg' => '成功',
-                'data' => $data,
-                'allCount' => $allCount,
-                'todayCount' => $todayCount,
-                'code' => 200
-            ]);
-        } else {
-            return response()->json([
-                'code' => 0,
-                'msg' => "无数据",
-            ], 200);
-        }
     }
 
- /*        $data = Betting::orderBy('bottom_pour_time', 'desc')->where('username', $user->username)->whereDate('bottom_pour_time', '>', $btime)->when($platform_name, function ($query) use ($platform_name) {
-            $query->where('platform_name', '=', $platform_name);
-        })->when($yesterday, function ($query) use ($yesterday) {
-            $query->whereDate('bottom_pour_time', '=', $yesterday);
-        })->get(
-            ['id', 'platform_name', 'game_name', 'bottom_pour', 'group_money', 'bottom_pour_time']
-        );
 
-        $todayCount = Betting::orderBy('bottom_pour_time', 'desc')->where('username', $user->username)->whereDate('bottom_pour_time', '>', $btime)->when($platform_name, function ($query) use ($platform_name) {
-            $query->where('platform_name', '=', $platform_name);
-        })->when($yesterday, function ($query) use ($yesterday) {
-            $query->whereDate('bottom_pour_time', '=', $yesterday);
-        })->selectRaw('DATE_FORMAT(bottom_pour_time,"%m-%d") as date,COUNT(id) as num ,SUM(bottom_pour) as bottom_pour,SUM(group_money) as group_money')
-            ->groupBy('date')->get();
+    if ($data) {
+        return response()->json([
+            'msg' => '成功',
+            'data' => $data,
+            'allCount' => $allCount,
+            'todayCount' => $todayCount,
+            'code' => 200
+        ]);
+    } else {
+        return response()->json([
+            'code' => 0,
+            'msg' => "无数据",
+        ], 200);
+    }
 
-        $allCount = Betting::orderBy('bottom_pour_time', 'desc')->where('username', $user->username)->whereDate('bottom_pour_time', '>', $btime)->when($platform_name, function ($query) use ($platform_name) {
-            $query->where('platform_name', '=', $platform_name);
-        })->when($yesterday, function ($query) use ($yesterday) {
-            $query->whereDate('bottom_pour_time', '=', $yesterday);
-        })->selectRaw('COUNT(id) as num ,SUM(bottom_pour) as bottom_pour,SUM(group_money) as group_money')
-            ->get();
 
-        if ($data) {
-            return response()->json([
-                'msg' => '成功',
-                'data' => $data,
-                'allCount' => $allCount,
-                'todayCount' => $todayCount,
-                'code' => 200
-            ]);
-        } else {
-            return response()->json([
-                'code' => 0,
-                'msg' => "无数据",
-            ], 200);
-        } */
     }
 }

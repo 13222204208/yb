b8e6dec87e779275f461987d900175e5d89e044e
yangpanda <ypp13222204208@gmail.com>
2020-08-12 10:55:36 +0800

    8/11

--- app/Http/Controllers/Api/YBCPApiController.php
+++ app/Http/Controllers/Api/YBCPApiController.php
@@ -184,8 +184,9 @@ class YBCPApiController extends Controller
         $url ='http://pull.shayexiang.com/202008/11/real/order/17.json';
         $json_string = file_get_contents($url);
         $json_string = '['.str_replace('}','},',$json_string).']';
-        return $json_string;
+        $json_string = str_replace(',]',']',$json_string);
+
         $data = json_decode($json_string,true);
-        return $json_string;
+        return $data;
     }
 }
